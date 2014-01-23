4cfdd33cbc86714e734fe1bad8447db56adc0b68
Patrick Lodder <patrick.lodder@intellibiz.org>
2014-01-23 22:44:46 +0100

    respect 80 char col width :-)

--- test/date_formatter.js
+++ test/date_formatter.js
@@ -8,14 +8,17 @@ vows.describe('Date Formatter').addBatch({
       return new Date(2014,0,20,14,25,25)
     }
   , "when encoded": {
-      "without hyphens": makeEncodeCase({hyphens: false}, '^\\d{8}T')
-    , "with hyphens": makeEncodeCase({hyphens: true}, '^\\d{4}[-]\\d{2}[-]\\d{2}T')
-    , "without colons": makeEncodeCase({colons: false}, 'T\\d{6}')
-    , "with colons": makeEncodeCase({colons: true}, 'T\\d{2}:\\d{2}:\\d{2}')
-    , "without offset": makeEncodeCase({offset: false}, 'T\\d{2}[:]?\\d{2}[:]?\\d{2}(\\.\\d{3})?$')
-    , "with offset": makeEncodeCase({offset: true}, '([+-]\\d{2}[:]\\d{2}|Z)$')
-    , "with milliseconds": makeEncodeCase({ms: true}, '\\.\\d{3}([+-]\\d{2}[:]\\d{2}|Z)?$')
-    , "without milliseconds": makeEncodeCase({ms: false}, 'T\\d{2}[:]?\\d{2}[:]?\\d{2}([+-]\\d{2}[:]\\d{2}|Z)?$')
+      "without hyphens": encodeCase({hyphens: false}, '^\\d{8}T')
+    , "with hyphens": encodeCase({hyphens: true}, '^\\d{4}[-]\\d{2}[-]\\d{2}T')
+    , "without colons": encodeCase({colons: false}, 'T\\d{6}')
+    , "with colons": encodeCase({colons: true}, 'T\\d{2}:\\d{2}:\\d{2}')
+    , "without offset": encodeCase({offset: false}, 'T\\d{2}[:]?\\d{2}[:]?'
+      + '\\d{2}(\\.\\d{3})?$')
+    , "with offset": encodeCase({offset: true}, '([+-]\\d{2}[:]\\d{2}|Z)$')
+    , "with milliseconds": encodeCase({ms: true}, '\\.\\d{3}([+-]\\d{2}[:]'
+      + '\\d{2}|Z)?$')
+    , "without milliseconds": encodeCase({ms: false}, 'T\\d{2}[:]?\\d{2}[:]?'
+      + '\\d{2}([+-]\\d{2}[:]\\d{2}|Z)?$')
     , "to local representation": {
         topic: function (d) {
           date_formatter.setOpts()
@@ -23,7 +26,8 @@ vows.describe('Date Formatter').addBatch({
           return date_formatter.encodeIso8601(d)
         }
       , "must return a properly formatted string": function (e, str) {
-          var reStr = '^\\d{4}[-]?\\d{2}[-]?\\d{2}T\\d{2}[:]?\\d{2}[:]?\\d{2}(\\.\\d{3})?([+-]\\d{2}[:]\\d{2})?$'
+          var reStr = '^\\d{4}[-]?\\d{2}[-]?\\d{2}T\\d{2}[:]?\\d{2}[:]?\\d{2}'
+          + '(\\.\\d{3})?([+-]\\d{2}[:]\\d{2})?$'
           assert.isNull(e)
           assert.isString(str)
           assert.match(str, new RegExp(reStr))
@@ -42,7 +46,8 @@ vows.describe('Date Formatter').addBatch({
           return date_formatter.encodeIso8601(d)
         }
       , "must return a properly formatted string": function (e, str) {
-          var reStr = '^\\d{4}[-]?\\d{2}[-]?\\d{2}T\\d{2}[:]?\\d{2}[:]?\\d{2}(\\.\\d{3})?Z$'
+          var reStr = '^\\d{4}[-]?\\d{2}[-]?\\d{2}T\\d{2}[:]?\\d{2}[:]?\\d{2}'
+          + '(\\.\\d{3})?Z$'
           assert.isNull(e)
           assert.isString(str)
           assert.match(str, new RegExp(reStr))
@@ -50,7 +55,13 @@ vows.describe('Date Formatter').addBatch({
       , "must match the correct time": function (str) {
           var offset = new Date().getTimezoneOffset()
           var round = (offset < 0) ? 'ceil' : 'floor'
-          var reStr = ['T[0]?', 14+Math[round](offset/60), '[:]?[0]?', 25+(offset%60), '[:]?25(\\.000)?Z'].join('')
+          var reStr = [
+            'T[0]?'
+          , 14+Math[round](offset/60)
+          , '[:]?[0]?'
+          , 25+(offset%60)
+          , '[:]?25(\\.000)?Z'
+          ].join('')
           assert.isString(str)
           assert.match(str, new RegExp(reStr))
         }
@@ -60,39 +71,54 @@ vows.describe('Date Formatter').addBatch({
   }
 }).addBatch({
   "When decoding": {
-    "YYYY-MM-DDTHH:mm:ss.mss": makeDecodeCase('2014-01-20T14:25:25.050', localDate(2014,0,20,14,25,25,50))
-  , "YYYY-MM-DDTHH:mm:ss": makeDecodeCase('2014-01-20T14:25:25', localDate(2014,0,20,14,25,25))
-  , "YYYY-MM-DDTHH:mm": makeDecodeCase('2014-01-20T14:25', localDate(2014,0,20,14,25))
-  , "YYYY-MM-DDTHH": makeDecodeCase('2014-01-20T14', localDate(2014,0,20,14))
-  , "YYYY-MM-DD": makeDecodeCase('2014-01-20', localDate(2014,0,20))
-  , "YYYYMMDDTHH:mm:ss": makeDecodeCase('20140120T14:25:25', localDate(2014,0,20,14,25,25))
-  , "YYYYMMDDTHHmmss": makeDecodeCase('20140120T142525', localDate(2014,0,20,14,25,25))
-  , "YYYYMMDDTHHmm": makeDecodeCase('20140120T1425', localDate(2014,0,20,14,25))
-  , "YYYYMMDD": makeDecodeCase('20140120', localDate(2014,0,20))
-  , "YYYY-MM-DDTHH:mm:ss.mssZ": makeDecodeCase('2014-01-20T14:25:25.050Z', '2014-01-20T14:25:25.050Z')
-  , "YYYY-MM-DDTHH:mm:ssZ": makeDecodeCase('2014-01-20T14:25:25Z', '2014-01-20T14:25:25.000Z')
-  , "YYYY-MM-DDTHHZ": makeDecodeCase('2014-01-20T14Z', '2014-01-20T14:00:00.000Z')
-  , "YYYY-MM-DDTHH:mm:ss.mss+hh:mm": makeDecodeCase('2014-01-20T14:25:25.000+09:30', function () {
-      var d = new Date('2014-01-20T14:25:25.000Z')
-      d.setUTCHours(d.getUTCHours() - 9)
-      d.setUTCMinutes(d.getUTCMinutes() - 30)
-      return d.toISOString()
-    })
-  , "YYYY-MM-DDTHH:mm:ss.mss+hhmm": makeDecodeCase('2014-01-20T14:25:25.000+0930', function () {
-      var d = new Date('2014-01-20T14:25:25.000Z')
-      d.setUTCHours(d.getUTCHours() - 9)
-      d.setUTCMinutes(d.getUTCMinutes() - 30)
-      return d.toISOString()
-    })
-  , "YYYY-MM-DDTHH:mm:ss.mss+hh": makeDecodeCase('2014-01-20T14:25:25.000+09', function () {
-      var d = new Date('2014-01-20T14:25:25.000Z')
-      d.setUTCHours(d.getUTCHours() - 9)
-      return d.toISOString()
-    })
+    "YYYY-MM-DDTHH:mm:ss.mss": decodeCase('2014-01-20T14:25:25.050'
+      , localDate(2014,0,20,14,25,25,50))
+  , "YYYY-MM-DDTHH:mm:ss": decodeCase('2014-01-20T14:25:25'
+      , localDate(2014,0,20,14,25,25))
+  , "YYYY-MM-DDTHH:mm": decodeCase('2014-01-20T14:25'
+      , localDate(2014,0,20,14,25))
+  , "YYYY-MM-DDTHH": decodeCase('2014-01-20T14'
+      , localDate(2014,0,20,14))
+  , "YYYY-MM-DD": decodeCase('2014-01-20'
+      , localDate(2014,0,20))
+  , "YYYYMMDDTHH:mm:ss": decodeCase('20140120T14:25:25'
+      , localDate(2014,0,20,14,25,25))
+  , "YYYYMMDDTHHmmss": decodeCase('20140120T142525'
+      , localDate(2014,0,20,14,25,25))
+  , "YYYYMMDDTHHmm": decodeCase('20140120T1425', localDate(2014,0,20,14,25))
+  , "YYYYMMDD": decodeCase('20140120', localDate(2014,0,20))
+  , "YYYY-MM-DDTHH:mm:ss.mssZ": decodeCase('2014-01-20T14:25:25.050Z'
+      , '2014-01-20T14:25:25.050Z')
+  , "YYYY-MM-DDTHH:mm:ssZ": decodeCase('2014-01-20T14:25:25Z'
+      , '2014-01-20T14:25:25.000Z')
+  , "YYYY-MM-DDTHHZ": decodeCase('2014-01-20T14Z', '2014-01-20T14:00:00.000Z')
+  , "YYYY-MM-DDTHH:mm:ss.mss+hh:mm": decodeCase('2014-01-20T14:25:25.000+09:30'
+      , function () {
+        var d = new Date('2014-01-20T14:25:25.000Z')
+        d.setUTCHours(d.getUTCHours() - 9)
+        d.setUTCMinutes(d.getUTCMinutes() - 30)
+        return d.toISOString()
+      }
+    )
+  , "YYYY-MM-DDTHH:mm:ss.mss+hhmm": decodeCase('2014-01-20T14:25:25.000+0930'
+      , function () {
+        var d = new Date('2014-01-20T14:25:25.000Z')
+        d.setUTCHours(d.getUTCHours() - 9)
+        d.setUTCMinutes(d.getUTCMinutes() - 30)
+        return d.toISOString()
+      }
+    )
+  , "YYYY-MM-DDTHH:mm:ss.mss+hh": decodeCase('2014-01-20T14:25:25.000+09'
+      , function () {
+        var d = new Date('2014-01-20T14:25:25.000Z')
+        d.setUTCHours(d.getUTCHours() - 9)
+        return d.toISOString()
+      }
+    )
   }
 }).export(module)
 
-function makeEncodeCase (opts, reStr) {
+function encodeCase (opts, reStr) {
   return {
     topic: function (d) {
       date_formatter.setOpts(opts)
@@ -107,7 +133,7 @@ function makeEncodeCase (opts, reStr) {
   }
 }
 
-function makeDecodeCase(str, check) {
+function decodeCase(str, check) {
   if (typeof(check) === 'function') check = check()
   return {
     topic: function () {
